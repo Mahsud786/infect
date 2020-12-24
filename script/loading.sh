bea2d798905cfd5b9178d7e759b6392505706069
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2020-12-24 04:27:48 -0800

    Add files via upload

---
+++ script/loading.sh
@@ -0,0 +1,3 @@
+z="
+";cz='clea';Nz='0m'\''';Gz='load';bz=''\''';dz='r';Sz='g ^_';Hz='ing ';Cz='    ';Dz=' '\''\0';gz='te ^';Bz=' -ne';Rz='adin';Fz=';32m';az='1;0m';ez='g co';Vz='###]';Pz='p 1';Jz='[###';Ez='33[1';kz='m'\''';Iz='^_^ ';fz='mple';jz='[1;0';Tz='^ [#';Xz=';0m'\''';Zz='033[';Yz=']\r\';iz='\033';Mz='3[1;';hz='_^\r';Lz='r\03';Oz='slee';Wz='\r\0';lz=' '\''\n';Uz='####';Az='echo';Kz='##]\';Qz=' '\''lo';
+eval "$Az$Bz$Cz$Cz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$Pz$z$Az$Bz$Cz$Cz$Cz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Ez$Xz$z$Oz$Pz$z$Az$Bz$Cz$Cz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Uz$Vz$Wz$Ez$Xz$z$Oz$Pz$z$Az$Bz$Cz$Cz$Cz$Qz$Rz$Sz$Tz$Uz$Uz$Uz$Yz$Zz$az$bz$z$Oz$Pz$z$Az$Bz$Cz$Cz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Uz$Uz$Uz$Yz$Zz$az$bz$z$Oz$Pz$z$cz$dz$z$Az$Bz$Cz$Cz$Cz$Qz$Rz$ez$fz$gz$hz$iz$jz$kz$z$Az$Bz$lz$bz"
